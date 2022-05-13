aa98fc14ed701c783fb902b545ddf8312a59f978
selva <selva.gmail.com>
2022-05-13 12:38:31 +0530

    new commit

--- DjangoLearnings/projects/views.py
+++ DjangoLearnings/projects/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 def project(request,id):
-    return render(request, 'single-project.html')
+    return render(request, 'projects/single-project.html')
 
 def pro(reques):
     return HttpResponse("fuckoff")
